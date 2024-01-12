7a680ee0e303a1e5af71f9347ba754d143db295b
Usmoxan Dev <101461756+Usmoxan@users.noreply.github.com>
2024-01-12 10:19:35 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,39 +1,52 @@
-import { useCallback, useState } from "react"
-import React from "react"
-import ItemList from "./components/item-list"
+import { useRef, useState } from "react";
+import React from "react";
 
 export default function App() {
-  const [counter, setCount] = useState(0)
-  const [active, setActive] = useState(true)
-  const onIncrement = () => setCount(prevState => prevState + 1)
-  const onToggle = () => setActive(prevState => !prevState)
-  const counterGenerate = useCallback(() => {
-    return new Array(counter).fill('').map((_, idx) => `counter ${idx + 1}`)
-  }, [counter])
-  const colors = {
-    fontWeight: "bold",
-    color: active ? 'green' : 'red'
-  }
+  const [text, setText] = useState("");
+  const [cvc, setDate] = useState("");
+  const cvcRef = useRef(null);
+  const dateRef = useRef(null);
+
+  const handleOnChange = (e) => {
+    const val = e.target.value;
+    setText(val);
+    if (val.length === 16) {
+      cvcRef.current.focus();
+    }
+  };
+  const handleOnCvcChange = (e) => {
+    const vals = e.target.value;
+    setDate(vals);
+    if (vals.length === 3) {
+      dateRef.current.focus();
+    }
+  };
+
   return (
-    <div className='w-50 mx-auto mb-5' >
-
-      <div className='border p-3 mt-5' >
-
-        <p className='text-center' style={colors}> User activeted{counter} </p>
-        <div className='d-flex justify-content-center' >
-
-          <button className='btn btn-success' onClick={onIncrement} >
-            Increase
-          </button>
-          <button className='btn btn-warning' onClick={onToggle}>
-            Toggle
-          </button>
-        </div>
-        <ItemList counterGenerate={counterGenerate} />
+    <div className="w-50 mx-auto mb-5">
+      <div className="border p-3 mt-5">
+        <input
+          type="text"
+          className="form-control"
+          onChange={handleOnChange}
+          value={text}
+          placeholder="Card number"
+        />
+        <input
+          type="text"
+          className="form-control mt-2"
+          ref={cvcRef}
+          onChange={handleOnCvcChange}
+          value={cvc}
+          placeholder="CVC code"
+        />
+        <input
+          type="text"
+          className="form-control mt-2"
+          ref={dateRef}
+          placeholder="Expiration date"
+        />
       </div>
-    </div >
+    </div>
   );
 }
-
-
-
